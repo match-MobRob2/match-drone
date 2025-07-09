939427ef5be8ddc0ea8bd832466002d6784db086
Luca <Luca0204@freenet.de>
2025-07-09 13:06:36 +0200

    Dinge

---
+++ match_models/install_models.sh
@@ -0,0 +1,13 @@
+cp misc/* ../PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/
+
+cp -r urdf/* ../PX4-Autopilot/Tools/simulation/gz/models/
+
+sed -i '/# \[22000, 22999\] Reserve for custom models/q' ../PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/CMakeLists.txt
+
+cat <<EOL >> ../PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/CMakeLists.txt
+    40011_gz_match_drohne
+   	40012_gz_match_drohne_lidar
+    40013_gz_match_drohne_fr_camera
+)
+EOL
+

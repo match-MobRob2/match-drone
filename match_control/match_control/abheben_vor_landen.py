d1e41ff4a40768db2dfe8d0ddf14b06dba8b9add
Luca <Luca0204@freenet.de>
2025-08-04 10:46:11 +0200

    Hi

--- match_control/match_control/abheben_vor_landen.py
+++ match_control/match_control/abheben_vor_landen.py
@@ -1,11 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from mavros_msgs.srv import CommandBool, SetMode, CommandTOL
+from mavros_msgs.srv import CommandBool, SetMode, CommandTOL, ParamSet
+from mavros_msgs.msg import ParamValue
 from geometry_msgs.msg import PoseStamped
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
 class SimpleDroneNode(Node):
     def __init__(self):
         super().__init__('simple_drone_node')
+
+        qos_profile = QoSProfile(
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+        )
 
         # Publisher
         self.local_pos_pub = self.create_publisher(PoseStamped, '/mavros/setpoint_position/local', 10)
@@ -15,6 +22,10 @@ class SimpleDroneNode(Node):
         self.set_mode_client = self.create_client(SetMode, '/mavros/set_mode')
         self.takeoff_client = self.create_client(CommandTOL, '/mavros/cmd/takeoff')
         self.land_client = self.create_client(CommandTOL, '/mavros/cmd/land')
+        self.set_parameters_client = self.create_client(ParamSet, '/mavros/param/set')
+
+        #Sub clients
+        self.get_position_client = self.create_subscription(PoseStamped, '/mavros/local_position/pose', self.position_callback, qos_profile)
 
         # Interner Status
         self.state = 0
@@ -28,6 +39,9 @@ class SimpleDroneNode(Node):
 
         # Loop-Timer 10 Hz
         self.timer = self.create_timer(0.1, self.control_loop)
+
+        self.set_parameter('MPC_XY_VEL_MAX', 4.0) # Set maximum horizontal velocity
+
 
     def control_loop(self):
         self.target_pose.header.stamp = self.get_clock().now().to_msg()
@@ -97,6 +111,21 @@ class SimpleDroneNode(Node):
         req.custom_mode = "OFFBOARD"
         future = self.set_mode_client.call_async(req)
         future.add_done_callback(lambda f: self.get_logger().info('SetMode result: {}'.format(f.result().mode_sent)))
+
+    def position_callback(self, msg):
+        self.get_logger().info('Current position: x={}, y={}, z={}'.format(
+            msg.pose.position.x, msg.pose.position.y, msg.pose.position.z))
+        
+
+    def set_parameter(self, parameter, value):
+        param = ParamSet.Request()
+        param.param_id = parameter
+        param.value = ParamValue()
+        param.value.real = value
+        param.value.integer = 0
+        future = self.set_parameters_client.call_async(param)
+        future.add_done_callback(lambda f: self.get_logger().info('Set parameter {} result: {}'.format(parameter, f.result().success)))
+
 
     def land(self):
         req = CommandTOL.Request()

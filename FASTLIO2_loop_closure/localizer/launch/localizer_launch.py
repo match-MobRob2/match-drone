80ad9eaa4aecef11204bd03548505acb9e38f668
Abderraouf <abderraouf7@hotmail.com>
2025-10-20 15:30:41 +0200

    init localization ws

---
+++ FASTLIO2_loop_closure/localizer/launch/localizer_launch.py
@@ -0,0 +1,59 @@
+import launch
+import launch_ros.actions
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+import os.path
+
+def generate_launch_description():
+    rviz_cfg = PathJoinSubstitution(
+        [FindPackageShare("localizer"), "rviz", "localizer.rviz"]
+    )
+    localizer_config_path = PathJoinSubstitution(
+        [FindPackageShare("localizer"), "config", "localizer.yaml"]
+    )
+    lio_config_path = PathJoinSubstitution(
+        [FindPackageShare("fastlio2"), "config", "lio.yaml"]
+    )
+
+    return launch.LaunchDescription(
+        [
+            # launch_ros.actions.Node(
+            #     package="fastlio2",
+            #     namespace="fastlio2",
+            #     executable="lio_node",
+            #     name="lio_node",
+            #     output="screen",
+            #     # parameters=[
+            #     #     {"config_path": lio_config_path.perform(launch.LaunchContext())}
+            #     # ],
+            #     parameters=[{"config_path": lio_config_path.perform(launch.LaunchContext()), "use_sim_time": True}]
+            # ),
+
+            launch_ros.actions.Node(
+                package="localizer",
+                namespace="localizer",
+                executable="localizer_node",
+                name="localizer_node",
+                output="screen",
+                # parameters=[
+                #     {
+                #         "config_path": localizer_config_path.perform(
+                #             launch.LaunchContext()
+                #         )
+                #     }],
+                parameters=[{"config_path": localizer_config_path.perform(launch.LaunchContext()), "use_sim_time": True}]
+            ),
+            launch_ros.actions.Node(
+                package="rviz2",
+                namespace="localizer",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+                arguments=["-d", rviz_cfg.perform(launch.LaunchContext())],
+                parameters=[{"use_sim_time": True}],
+            )
+        ]
+    )
